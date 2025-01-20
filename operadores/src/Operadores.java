bdb0f9d190ea3ef94ac0b3adc0de672f0d026208
bhclira <brenohencastro@gmail.com>
2025-01-20 18:57:40 -0300

    adicionar exemplos de operadores ternários e relacionais em Operadores.java

--- operadores/src/Operadores.java
+++ operadores/src/Operadores.java
@@ -10,7 +10,7 @@ public class Operadores {
         boolean casado = true;
         Date dataNascimento = new Date();
 
-        // operadores aritmeticos : + - * / %
+    // operadores aritmeticos : + - * / %
 
         double soma = 10.5 + 15.7;
         int subtracao = 113 - 25;
@@ -19,13 +19,13 @@ public class Operadores {
         int modulo = 18 % 3; // resto da divisão
         double resultado = (10 * 7) + (20 / 7);
 
-        // o operador + é usado para concatenar strings
+    // o operador + é usado para concatenar strings
 
         String nomeCompleto = nome + " Castro";
 
         System.out.println(nomeCompleto);
         
-        // operadores unários: ++ --
+    // operadores unários: ++ --
 
         int num1 = +10;
         int num2 = -10;
@@ -37,7 +37,7 @@ public class Operadores {
         
         System.out.println(num1);
 
-        // incremento e decremento de números
+    // incremento e decremento de números
 
         num2 = 5;
         
@@ -48,7 +48,7 @@ public class Operadores {
 
         System.out.println(num2); // primeiro imprime o valor e depois incrementa
 
-        // negando expressões booleanas
+    // negando expressões booleanas
 
         boolean logico = true;
 
@@ -57,5 +57,72 @@ public class Operadores {
         logico  = !logico; // inverte o valor da variavel
 
         System.out.println(logico);
+
+
+    // operadores ternários
+
+
+        int a, b;
+        
+        a = 10;
+        b = 10;
+
+        String resultado2 = "";
+        
+        if (a==b)
+            resultado2 = "verdadeiro";
+        else
+            resultado2 = "é falso";
+        
+            System.out.println(resultado2);
+        
+        // <exp condicional> ? <caso cond seja true> : <caso cond seja false>
+        
+        String resultado3 = a==b ? "verdadeiro" : "é falso";
+
+        System.out.println(resultado3);
+
+        // ternario com inteiros
+        int resultado4 = a == b ? 1 : 0;
+
+        System.out.println(resultado4);
+
+    // operadores relacionais: realiza comparações entre valores no fluxo de execução
+
+        // == != > < >= <= retorna um valor booleano true or false
+
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+
+        if (numero1 < numero2) {
+            System.out.println("a nossa condição é verdadeira");
+        } else {
+            System.out.println("a nossa condição é falsa");
+        }
+
+        System.out.println("Numero1 é igual a numero2? " + simNao);
+
+        simNao = numero1 != numero2;
+
+        System.out.println("Numero1 é diferente de numero2? " + simNao);
+
+        simNao = numero1 > numero2;
+
+        System.out.println("Numero1 é maior que numero2? " + simNao);
+
+            // comparando textos ou objetos
+
+        String nomeUm = "BRENO";
+        String nomeDois = new String ("BRENO");
+
+        System.out.println(nomeUm == nomeDois); // false dois objetos na memoria stake vs heap
+
+        System.out.println(nomeUm.equals(nomeDois)); // true compara o conteudo para objetos
+        
+
+
+
     }
 }
