2dfa3b512c2051ec195d2d68506fcb5123bf3f1d
bhclira <brenohencastro@gmail.com>
2025-01-20 18:00:40 -0300

    adicionar exemplos de tipos de variáveis e constantes em TiposVariaveis.java

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,33 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        byte idade = 21;
+        short quantidadeAlunos = 100;
+        int cep = 12345678;
+        long cpf = 12345678901L;
+        float salario = 2000.0f;
+        double salarioMinimo = 2000.0;
+
+        // CUIDADO! Java é fortemente tipado, ou seja, não é possível atribuir um valor de um tipo a uma variável de outro tipo!
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+        
+        // variáveis vs constantes
+        // conceito constante: valor que não pode ser alterado
+        // SINTAXE: final TIPO NOME = VALOR;
+
+        
+        final double VALOR_DE_PI = 3.14;
+
+        // VALOR_DE_PI = 3.14; // erro de compilação
+
     }
 }
