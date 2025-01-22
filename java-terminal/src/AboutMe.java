cf049046fa5627b6cbd35ffe84bb2eb77d803f67
bhclira <brenohencastro@gmail.com>
2025-01-22 11:55:15 -0300

    Alterar entrada de dados para uso de Scanner em vez de argumentos de linha de comando na classe AboutMe

--- java-terminal/src/AboutMe.java
+++ java-terminal/src/AboutMe.java
@@ -1,15 +1,26 @@
+import java.util.Scanner;
+
 public class AboutMe {
     public static void main(String[] args) {
-        // OS ARGUMENTOS COMEÇAM COM INDICE 0
-        String nome = args[0];
-        String sobrenome = args[1];
-        int idade = Integer.valueOf(args[2]);
-        double altura = Double.valueOf(args[3]);
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.println("Digite seu sobrenome: ");
+        String sobrenome = scanner.nextLine();
+
+        System.out.println("Digite sua idade: ");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite sua altura: ");
+        double altura = scanner.nextDouble();
+
+       // imprimindo os dados
         System.out.println("Olá, me chamo " + nome + " " + sobrenome);
         System.out.println("Tenho " + idade + " anos");
         System.out.println("Minha altura é " + altura + "m");
-        
 
     }
     
