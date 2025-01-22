ba7f4fbdac31d5d790a31fbfce29cfd728ed4c94
bhclira <brenohencastro@gmail.com>
2025-01-21 21:04:14 -0300

    eh nois

---
+++ java-terminal/src/Calculadora.java
@@ -0,0 +1,26 @@
+/**
+ * A simple calculator class that prints "Hello, World!" to the console.
+ * 
+ * @author Breno
+ * @version 1.0
+ * @since 2023
+ */
+public class Calculadora {
+    /**
+ * Realiza a soma de dois números inteiros.
+ *
+ * @param a o primeiro número inteiro a ser somado
+ * @param b o segundo número inteiro a ser somado
+ * @return a soma dos dois números inteiros
+ */
+    public int somar(int a, int b) {
+            /**
+     * Este método recebe dois números inteiros como entrada e retorna a soma deles.
+     *
+     * @param a o primeiro número inteiro a ser somado
+     * @param b o segundo número inteiro a ser somado
+     * @return a soma dos dois números inteiros
+     */
+        return a + b;
+    }
+}
---
+++ java-terminal/src/Comentarios.java
@@ -0,0 +1,14 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        // Olá, eu sou um comentário de linha
+
+        /*
+         * Olá, eu sou um comentário de bloco
+         */
+
+         /**
+          * Olá, eu sou um comentário de documentação
+          */
+
+    }
+}
