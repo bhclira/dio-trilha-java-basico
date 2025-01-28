3e6fabfbdb9978853a244e73141057813fb308ad
bhclira <brenohencastro@gmail.com>
2025-01-28 11:10:51 -0300

    Adicionar configuração do projeto e estrutura inicial para controle de fluxo em Java

---
+++ controle-de-fluxo/src/ControleFluxo.java
@@ -0,0 +1,18 @@
+/* RESUMO
+Estrutura de controle de fluxo
+if, else if, else
+estruturas de repetição
+for, while, do while
+switch case
+break, continue
+estruturas de excessão
+try, catch, finally, throw, throws
+ */
+
+public class ControleFluxo {
+    public static void main(String[] args) throws Exception {
+        
+        
+
+    }
+}
