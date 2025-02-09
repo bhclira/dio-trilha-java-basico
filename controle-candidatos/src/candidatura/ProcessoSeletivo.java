b6bef60b2f8d269dd5b80ab3b415edddf5b87609
bhclira <brenohencastro@gmail.com>
2025-02-09 20:29:38 -0300

    Adicionar lógica de seleção de candidatos com base no salário pretendido

--- controle-candidatos/src/candidatura/ProcessoSeletivo.java
+++ controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -1,12 +1,34 @@
 package candidatura;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
         System.out.println("Processo Seletivo");
-        
-        analisarCandidato(1900.0);
-        analisarCandidato(2200.0);
-        analisarCandidato(2000.0);
+        selecaoCandidatos();
+    }
+
+    static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
+    }
+
+    static void selecaoCandidatos () {
+        String [] candidatos = {"João", "Maria", "José", "Ana", "Carlos", "Bruno", "Rodrigo", "Jonas", "Leonardo", "Pedro"};
+
+        int candidatosSelecionados = 0;
+        int candidatoAtual = 0;
+        double salarioBase = 2000.0;
+
+        while ((candidatosSelecionados < 5) &&  (candidatoAtual < candidatos.length)) {
+            String candidato = candidatos[candidatoAtual];
+            double salarioPretendido = valorPretendido();
+            
+            System.out.println("Analisando candidato: " + candidato + " Solicitou esse valor de salário: " + String.format("%.2f", salarioPretendido));
+            if(salarioBase >= salarioPretendido) {
+                System.out.println("O candidato " + candidato + " foi selecionado para a vaga");
+                candidatosSelecionados++;
+            }
+            candidatoAtual++;
+        }
     }
 
     static void analisarCandidato (double salarioPretendido) {
